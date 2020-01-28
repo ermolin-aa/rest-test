515c909fdbffbc7f31ad07c6485bb7bafbcefb38
Alexey Yermolin <ermolin@vtb.ru>
2020-01-28 16:58:33 +0300

    Minor additions

--- src/main/java/com/example/demo/contoller/QuizController.java
+++ src/main/java/com/example/demo/contoller/QuizController.java
@@ -3,6 +3,7 @@ package com.example.demo.contoller;
 import com.example.demo.domain.Quiz;
 import com.example.demo.service.QuizService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,10 +30,21 @@ public class QuizController {
     private final QuizService quizService;
 
     @GetMapping
+
     public ResponseEntity<List<Quiz>> getAllQuizzes(
             @RequestParam(defaultValue = "0") Integer pageNum,
             @RequestParam(defaultValue = "5") Integer pageSize,
-            @RequestParam(defaultValue = "id") String sortBy) {
+            @RequestParam(defaultValue = "title") String sortBy,
+            @RequestParam(defaultValue = "", required = false) String title,
+            @RequestParam(defaultValue = "true") Boolean active,
+            @RequestParam(defaultValue = "", required = false) String beginDate,
+            @RequestParam(defaultValue = "", required = false) String endDate) {
+
+        System.out.println("!!!");
+        System.out.println(title.isEmpty());
+        System.out.println(beginDate.isEmpty());
+        System.out.println(endDate.isEmpty());
+
 
         List<Quiz> quizzes = quizService.findAll(pageNum, pageSize, sortBy);
 
--- src/main/java/com/example/demo/service/QuizService.java
+++ src/main/java/com/example/demo/service/QuizService.java
@@ -35,4 +35,6 @@ public class QuizService {
             return new ArrayList<>();
         }
     }
+
+
 }
